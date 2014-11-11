4be8d3c778e03d2dcc7609efe9c92e84af60e14a
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-11 12:55:46 +0100

    Initial commit

---
+++ files/approve_pairing_request/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m],n,_(o,p,q,r,s,t,u,_(v,w,x,y,z,_(A,B,C,D),E,null,F,y,G,y,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(),S,_(),T,_(U,[])),V,_(),W,_());}; 
+var b="url",c="approve_pairing_request.html",d="generationDate",e=new Date(1414663107944.6),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="Name",k="FirstName",l="LastName",m="EmailAddress",n="page",o="packageId",p="c19fb7c5254f482483a10e17e0e45a3c",q="type",r="Axure:Page",s="name",t="Approve Pairing Request",u="style",v="baseStyle",w="627587b6038d43cca051c114ac41ad32",x="pageAlignment",y="near",z="fill",A="fillType",B="solid",C="color",D=0xFFFFFFFF,E="image",F="imageHorizontalAlignment",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="interactionMap",T="diagram",U="objects",V="masters",W="objectPaths";
+return _creator();
+})());
---
+++ files/wallet_login/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m],n,_(o,p,q,r,s,t,u,_(v,w,x,y,z,_(A,B,C,D),E,null,F,y,G,y,H,I,J,null,K,L,M,N,O,P,Q,L),R,_(),S,_(),T,_(U,[])),V,_(),W,_());}; 
+var b="url",c="wallet_login.html",d="generationDate",e=new Date(1414663107959.67),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="Name",k="FirstName",l="LastName",m="EmailAddress",n="page",o="packageId",p="87628394dcb342268dc721eea8f28ae2",q="type",r="Axure:Page",s="name",t="Wallet Login",u="style",v="baseStyle",w="627587b6038d43cca051c114ac41ad32",x="pageAlignment",y="near",z="fill",A="fillType",B="solid",C="color",D=0xFFFFFFFF,E="image",F="imageHorizontalAlignment",G="imageVerticalAlignment",H="imageRepeat",I="auto",J="favicon",K="sketchFactor",L="0",M="colorStyle",N="appliedColor",O="fontName",P="Applied Font",Q="borderWidth",R="adaptiveStyles",S="interactionMap",T="diagram",U="objects",V="masters",W="objectPaths";
+return _creator();
+})());
