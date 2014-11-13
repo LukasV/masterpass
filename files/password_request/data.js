16181566cfcc3b449cf6786666aa1127f9aa4b5f
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-13 16:27:49 +0100

    new bundle

---
+++ files/password_request/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n],o,_(p,q,r,s,t,u,v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="password_request.html",d="generationDate",e=new Date(1415888381031.5),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="Name",k="FirstName",l="LastName",m="EmailAddress",n="Flavour",o="page",p="packageId",q="43e5a3922c684852b9a273ce01bcc452",r="type",s="Axure:Page",t="name",u="Password Request",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
