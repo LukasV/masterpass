69fe9705c5c70b6bbcd154a64efad8b7b5e4dc03
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-17 18:23:45 +0100

    Bluetooth add

---
+++ data/app.js
@@ -0,0 +1,14 @@
+﻿(function ($) {
+    $( document ).ready(function() {
+        console.log($("html").html())
+        var bluetoothSerial = bluetoothSerial || null;
+//        alert($("#base").html())
+        if(bluetoothSerial){
+            $("#base").append("<p>Bluetooth ready!</p>");
+//            alert("Bluetooth ready!");
+        } else {
+            $("#base").append("<p>Bluetooth unavailable!</p>");
+//            alert("Bluetooth unavailable!");
+        }
+    });
+}(jQuery));
