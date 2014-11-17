04cee54d09f86143c545b13c3d7d54e1c4c94b45
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-17 20:32:50 +0100

    more fiddling

--- data/app.js
+++ data/app.js
@@ -1,6 +1,5 @@
 ﻿(function ($) {
     $( document ).ready(function() {
-        console.log($("html").html())
         var bluetoothSerial = bluetoothSerial || null;
 //        alert($("#base").html())
         if(bluetoothSerial){
@@ -11,24 +10,51 @@
 //            alert("Bluetooth unavailable!");
         }
     });
-    document.addEventListener("deviceready", onDeviceReady, false);
+/*    document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
-/*        var element = document.getElementById('base');
-        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
-                            'Device Cordova: '  + device.cordova  + '<br />' +
-                            'Device Platform: ' + device.platform + '<br />' +
-                            'Device UUID: '     + device.uuid     + '<br />' +
-                            'Device Version: '  + device.version  + '<br />';
-        var btstatus = document.getElementById('status');
-        btstatus.innerHTML = "Getting bluetooth information";
-        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);*/
-        console.log($("html").html())
+//        var element = document.getElementById('base');
+//        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+//                            'Device Cordova: '  + device.cordova  + '<br />' +
+//                            'Device Platform: ' + device.platform + '<br />' +
+//                            'Device UUID: '     + device.uuid     + '<br />' +
+//                            'Device Version: '  + device.version  + '<br />';
+//        var btstatus = document.getElementById('status');
+//        btstatus.innerHTML = "Getting bluetooth information";
+//        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
+        alert("Device Ready!")
+//        console.log($("html").html())
         var bluetoothSerial = bluetoothSerial || null;
         if(bluetoothSerial){
-            $("#base").append("<p>Bluetooth ready!</p>");
+            $("#base").append("<p>Device ready<br>Bluetooth ready!</p>");
         } else {
-            $("#base").append("<p>Bluetooth unavailable!</p>");
+            $("#base").append("<p>Device ready<br>Bluetooth unavailable!</p>");
         }
-    }
+    }*/
+    var macAddress = "00:13:EF:00:08:8B";
+    var app = {
+        initialize: function() {
+        this.bindEvents();
+        },
+        bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+        onDeviceReady: function() {
+        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+        },
+        onConnect: function() {
+        bluetoothSerial.subscribe("\n", app.onMessage, app.subscribeFailed);
+        statusDiv.innerHTML="Connected to " + macAddress + ".";
+        },
+        onDisconnect: function() {
+        alert("Disconnected");
+        statusDiv.innerHTML="Disconnected.";
+        },
+        onMessage: function(data) {
+        counter.innerHTML = data;
+        },
+        subscribeFailed: function() {
+        alert("subscribe failed");
+        }
+    };
 
 }(jQuery));
