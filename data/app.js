a329dfa0bfd7a3f8870239f4c9a306d441c501bc
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-17 18:35:43 +0100

    Bluetooth fiddle

--- data/app.js
+++ data/app.js
@@ -11,4 +11,24 @@
 //            alert("Bluetooth unavailable!");
         }
     });
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+/*        var element = document.getElementById('base');
+        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+        var btstatus = document.getElementById('status');
+        btstatus.innerHTML = "Getting bluetooth information";
+        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);*/
+        console.log($("html").html())
+        var bluetoothSerial = bluetoothSerial || null;
+        if(bluetoothSerial){
+            $("#base").append("<p>Bluetooth ready!</p>");
+        } else {
+            $("#base").append("<p>Bluetooth unavailable!</p>");
+        }
+    }
+
 }(jQuery));
