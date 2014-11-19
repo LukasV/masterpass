774ffcb581e4fd533aa96f613ce881919e8e6ce0
Lukas Vacovsky <luckyluke@luckyluke.eu>
2014-11-19 15:08:00 +0100

    Clean up

--- www/data/app.js
+++ www/data/app.js
@@ -1,60 +1,48 @@
-﻿(function ($) {
-    $( document ).ready(function() {
-        var bluetoothSerial = bluetoothSerial || null;
-//        alert($("#base").html())
-        if(bluetoothSerial){
-            $("#base").append("<p>Bluetooth ready!</p>");
-//            alert("Bluetooth ready!");
-        } else {
-            $("#base").append("<p>Bluetooth unavailable!</p>");
-//            alert("Bluetooth unavailable!");
-        }
-    });
-/*    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-//        var element = document.getElementById('base');
-//        element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
-//                            'Device Cordova: '  + device.cordova  + '<br />' +
-//                            'Device Platform: ' + device.platform + '<br />' +
-//                            'Device UUID: '     + device.uuid     + '<br />' +
-//                            'Device Version: '  + device.version  + '<br />';
-//        var btstatus = document.getElementById('status');
-//        btstatus.innerHTML = "Getting bluetooth information";
-//        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
-        alert("Device Ready!")
-//        console.log($("html").html())
-        var bluetoothSerial = bluetoothSerial || null;
-        if(bluetoothSerial){
-            $("#base").append("<p>Device ready<br>Bluetooth ready!</p>");
-        } else {
-            $("#base").append("<p>Device ready<br>Bluetooth unavailable!</p>");
-        }
-    }*/
-    var macAddress = "00:13:EF:00:08:8B";
-    var app = {
-        initialize: function() {
-        this.bindEvents();
-        },
-        bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        },
-        onDeviceReady: function() {
-        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
-        },
-        onConnect: function() {
-        bluetoothSerial.subscribe("\n", app.onMessage, app.subscribeFailed);
-        statusDiv.innerHTML="Connected to " + macAddress + ".";
-        },
-        onDisconnect: function() {
-        alert("Disconnected");
-        statusDiv.innerHTML="Disconnected.";
-        },
-        onMessage: function(data) {
-        counter.innerHTML = data;
-        },
-        subscribeFailed: function() {
-        alert("subscribe failed");
-        }
-    };
+﻿//var macAddress = "00:13:EF:00:08:8B"; // Nevim
+//var macAddress = "8C:3A:E3:6C:32:31"; // Ja
+var macAddress = "E0:63:E5:9E:5E:26"; // Lin - almost working! (it thinks for a while)
+//var macAddress = "20:54:76:9C:9A:64"; // Lin BT
+//var macAddress = "40:0E:85:56:CF:FC"; // Burt
+//var macAddress = "34:51:C9:39:D9:43"; // Standovo iPad
+//var macAddress = "2c1fe0f635c3a96f38324794a8959d8f2ba4df14"; // Standovo iPad
 
-}(jQuery));
+
+var app = {
+    initialize: function() {
+//        alert("initialize")
+        this.bindEvents();
+    },
+    bindEvents: function() {
+//        alert("bindEvents")
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        $("#statusDiv").html("<p>Device ready</p>");
+        $("#statusDiv").append("<p> "+bluetoothSerial+"</p>");
+        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+//        bluetoothSerial.connectInsecure(macAddress, app.onConnect, app.onDisconnect);
+    },
+    doConnect: function() {
+        $("#statusDiv").html("<p>Trying to connect...</p>");
+        bluetoothSerial.connect(macAddress, app.onConnect, app.onDisconnect);
+    },
+    onConnect: function() {
+        $("#statusDiv").append("Connected!");
+        $("#statusDiv").append("Connected to " + macAddress + ".");
+        bluetoothSerial.subscribe("\n", app.onMessage, app.subscribeFailed);
+    },
+    onDisconnect: function() {
+//      alert("Disconnected");
+        $("#statusDiv").append("Disconnected.");
+    },
+    onMessage: function(data) {
+        counter.innerHTML = data;
+    },
+    subscribeFailed: function() {
+        alert("subscribe failed");
+    }
+};
+
+$( document ).ready(function() {
+    $("#connect").click(app.doConnect);
+});
